de51a2bb9c1cb49d4553a8ee1a4e0d3df301c758
joshbae119 <mclceo119@gmail.com>
2024-12-26 09:33:19 +0900

    2024.12.26 수업전 푸시

--- src/test/java/com/springboot/biz/BoardTestApplicationTests.java
+++ src/test/java/com/springboot/biz/BoardTestApplicationTests.java
@@ -16,7 +16,7 @@ import com.springboot.biz.question.QuestionService;
 class BoardTestApplicationTests {
 	
 	@Autowired
-	private QuestionRepository questionRepository;
+	private QuestionService questionService;
 	
 	@Autowired
 	private AnswerRepository answerRepository;
@@ -26,11 +26,18 @@ class BoardTestApplicationTests {
 	void contextLoads() {
 		
 		
-		Question q1 = new Question();
-		q1.setSubject("게시판을 만드는 방법은?");
-		q1.setContent("매우 쉽습니다.");
-		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1);
+//		Question q1 = new Question();
+//		q1.setSubject("게시판을 만드는 방법은3333?");
+//		q1.setContent("매우 쉽습니다3333.");
+//		q1.setCreateDate(LocalDateTime.now());
+//		this.questionRepository.save(q1);
+
+//		//DB에 데이터 대량 넣기
+//		for (int i=0; i<=300 ; i++) {
+//			String subject = String.format("테스트 데이트입니다[%03d]", i);
+//			String content = "내용 없음";
+//			this.questionService.create(subject, content);
+//		}
 
 		
 		
